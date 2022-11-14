bc9774f56a6ad2e3c7d012d76aa05dbbf3d01c5e
Alexandra <alexandra@Alexandras-MacBook-Pro.local>
2022-11-14 13:31:34 -0700

    connected to mySql database

--- server.js
+++ server.js
@@ -4,10 +4,30 @@ const PORT = process.env.PORT || 3001 // port online
 
 const app = express()
 
+const mysql = require('mysql2') // requiring in order to connect to MySQL database
+
 //^------ Middleware functions ----------- 
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 //^---------------------------------------
+
+//** FUNCTION - connects the application to the MySQL database */
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Query the database to test the connection
+db.query(`SELECT * FROM candidates`, (err, rows) => { // This wil show each row in the database as an array of objects when console.logged to console
+    console.log(rows)
+})
 
 
 app.use((req, res) => {
